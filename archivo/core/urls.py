5bad4c3e9dc2ef7e19d9dbbf15c9bdff7030515c
Marcos Federico Mandrille <mmandrille@gmail.com>
2018-11-28 18:00:32 -0300

    Se agrego funcionalidad de organismos

--- archivo/core/urls.py
+++ archivo/core/urls.py
@@ -6,6 +6,7 @@ app_name = 'corearchivo'
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     path('etiqueta/<int:etiqueta_id>/', views.mostrar_archivos_etiqueta, name='mostrar_archivos_etiqueta'),
+    path('organismo/<int:organismo_id>/', views.mostrar_archivos_organismo, name='mostrar_archivos_organismo'),
     path('repositorio/<int:archivo_id>/', views.mostrar_archivo, name='mostrar_archivo'),
     path('repositorio/<str:archivo_nombre>/', views.mostrar_archivo_nombre, name='mostrar_archivo_nombre'),
 ]
--- archivo/core/views.py
+++ archivo/core/views.py
@@ -1,11 +1,11 @@
 from django.http import Http404
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.db.models import Q
+from django.db.models import Q, Count
 #Import incluidos
 from taggit.models import Tag
 #Import Personales
-from .models import Archivo
+from .models import Archivo, obtener_organismos
 from .forms import BuscarForm
 
 #@login_required
@@ -17,26 +17,75 @@ def home(request):
             #Realizamos la busqueda
             archivos = Archivo.objects.filter(
                 Q(nombre__icontains=form.cleaned_data['texto'])|
-                Q(etiquetas__icontains=form.cleaned_data['texto'])|
-                Q(resumen__icontains=form.cleaned_data['texto']))
+                Q(etiquetas__name=form.cleaned_data['texto'])|
+                Q(resumen__icontains=form.cleaned_data['texto'])).distinct()
             buscado = True
     else:
         archivos = Archivo.objects.order_by('fecha_aprobacion')[:12]
         buscado = False
+    #ETIQUETAS
     #Obtenemos las 5 tags mas usadas
     etiquetas = Archivo.etiquetas.most_common()[:5]
+    #ORGANISMOS
+    #obtenemos los 10 Organismos mas usados
+    ranking_organismos = Archivo.objects.values("organismo").annotate(count=Count('organismo')).order_by("-count")[:10]
+    #Obtenemos desde el sitio de organigrama todos los organismos > Ver models.py
+    organismos = obtener_organismos()
+    #lista_organismos= [organismo for organismo in organismos if organismo[0] == ranking_organismos[0]['organismo']]
+    lista_organismos=   [organismo #SI, acabas de ver la listcompression mas loca de la historia xD
+                        for organismo_ranking in [ranking['organismo']
+                        for ranking in ranking_organismos]
+                        for organismo in organismos
+                        if organismo[0] == organismo_ranking]
+    print(lista_organismos)
     #Obtenemos form de busqueda
     form = BuscarForm
     #Enviamos la pagina
-    return render(request, 'home.html', {'archivos': archivos, 'etiquetas': etiquetas, 'form': form, 'buscado': buscado })
+    return render(request, 'home.html', {'archivos': archivos, 'etiquetas': etiquetas, 'organismos': lista_organismos, 'form': form, 'buscado': buscado })
 
 def mostrar_archivos_etiqueta(request, etiqueta_id):
     #Obtenemos etiqueta
     etiqueta = Tag.objects.get(pk=etiqueta_id)
     #Obtenemos Archivos Taggeados
     archivos = Archivo.objects.filter(etiquetas=etiqueta)
+    #ETIQUETAS
     #Obtenemos las 5 tags mas usadas
     etiquetas = Archivo.etiquetas.most_common()[:5]
+    #ORGANISMOS
+    #obtenemos los 10 Organismos mas usados
+    ranking_organismos = Archivo.objects.values("organismo").annotate(count=Count('organismo')).order_by("-count")[:10]
+    #Obtenemos desde el sitio de organigrama todos los organismos > Ver models.py
+    organismos = obtener_organismos()
+    #lista_organismos= [organismo for organismo in organismos if organismo[0] == ranking_organismos[0]['organismo']]
+    lista_organismos=   [organismo #SI, acabas de ver la listcompression mas loca de la historia xD
+                        for organismo_ranking in [ranking['organismo']
+                        for ranking in ranking_organismos]
+                        for organismo in organismos
+                        if organismo[0] == organismo_ranking]
+    print(lista_organismos)
+    #Obtenemos form de busqueda
+    form = BuscarForm
+    #Enviamos la pagina
+    return render(request, 'home.html', {'archivos': archivos, 'etiquetas': etiquetas, 'form': form, 'buscado': False })
+
+def mostrar_archivos_organismo(request, organismo_id):
+    #Obtenemos Archivos del organismo
+    archivos = Archivo.objects.filter(organismo=organismo_id)
+    #ETIQUETAS
+    #Obtenemos las 5 tags mas usadas
+    etiquetas = Archivo.etiquetas.most_common()[:5]
+    #ORGANISMOS
+    #obtenemos los 10 Organismos mas usados
+    ranking_organismos = Archivo.objects.values("organismo").annotate(count=Count('organismo')).order_by("-count")[:10]
+    #Obtenemos desde el sitio de organigrama todos los organismos > Ver models.py
+    organismos = obtener_organismos()
+    #lista_organismos= [organismo for organismo in organismos if organismo[0] == ranking_organismos[0]['organismo']]
+    lista_organismos=   [organismo #SI, acabas de ver la listcompression mas loca de la historia xD
+                        for organismo_ranking in [ranking['organismo']
+                        for ranking in ranking_organismos]
+                        for organismo in organismos
+                        if organismo[0] == organismo_ranking]
+    print(lista_organismos)
     #Obtenemos form de busqueda
     form = BuscarForm
     #Enviamos la pagina
