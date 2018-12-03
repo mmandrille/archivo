796aed8fdad3d4786a1398a7ae73e056d057311b
servidor <servidor@DjangoGob.DjangoGob>
2018-12-03 12:40:08 -0300

    Se realizaron cambios varios

--- archivo/gunicorn.sh
+++ archivo/gunicorn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /opt/archivo
-source venv/bin/archivo
+source venv/bin/activate
 cd /opt/archivo/archivo
-gunicorn archivo.wsgi -t 600 -b 127.0.0.1:8000 -w 6 --user=servidor --group=servidor --log-file=/opt/archivo/gunicorn.log 2>>/opt/archivo/gunicorn.log
+gunicorn archivo.wsgi -t 600 -b 127.0.0.1:8006 -w 6 --user=servidor --group=servidor --log-file=/opt/archivo/gunicorn.log 2>>/opt/archivo/gunicorn.log
 
